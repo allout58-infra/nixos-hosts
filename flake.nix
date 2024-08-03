b3616124e870cd9e1a280e33f776d3d08b2d785b
James Hollowell <jamesthollowell@gmail.com>
2024-08-03 11:26:14 -0400

    feat: initial copy from nixos-media

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "All my common nix hosts";
+
+  # Inputs
+  # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
+
+  # The release branch of the NixOS/nixpkgs repository on GitHub.
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  inputs.nixpkgs-me.url = "github:allout58/nixpkgs";
+
+  # region AgeNix
+  inputs.agenix.url = "github:ryantm/agenix";
+  # optional, not necessary for the module
+  inputs.agenix.inputs.nixpkgs.follows = "nixpkgs";
+  # optionally choose not to download darwin deps (saves some resources on Linux)
+  inputs.agenix.inputs.darwin.follows = "";
+  # endregion
+
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager/release-24.05";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  inputs.nixos-common = {
+    url = "github:allout58-infra/nixos-common";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  # It is also possible to "inherit" an input from another input. This is useful to minimize
+  # flake dependencies. For example, the following sets the nixpkgs input of the top-level flake
+  # to be equal to the nixpkgs input of the nixops input of the top-level flake:
+  # inputs.nixpkgs.url = "nixpkgs";
+  # inputs.nixpkgs.follows = "nixops/nixpkgs";
+
+  # Work-in-progress: refer to parent/sibling flakes in the same repository
+  # inputs.c-hello.url = "path:../c-hello";
+
+  outputs =
+    all @ { self
+    , nixpkgs
+    , agenix
+    , nixos-common
+    , nixpkgs-me
+    , home-manager
+    , ...
+    }:
+    {
+      # Used with `nixos-rebuild --flake .#<hostname>`
+      # nixosConfigurations."<hostname>".config.system.build.toplevel must be a derivation
+      nixosConfigurations = {
+
+        jhollowell-laptop = nixpkgs.lib.nixosSystem rec {
+          system = "x86-64";
+          specialArgs = {
+            pkgs-me = import nixpkgs-me { inherit system; };
+          };
+          modules = [
+            ./configuration.nix
+            agenix.nixosModules.default
+            nixos-common.nixosModules.users
+            nixos-common.nixosModules.workloads.ssh
+            nixos-common.nixosModules.env.common
+            nixos-common.nixosModules.net.firewall
+            nixos-common.nixosModules.net.tailscale
+            nixos-common.nixosModules.workloads.diag
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+
+              home-manager.users.jhollowell = nixos-common.nixosModules.home-manager.jhollowell;
+            }
+          ];
+        };
+      };
+
+      # format the nix code in this flake
+      # alejandra is a nix formatter with a beautiful output
+      formatter.x86-64 = nixpkgs.legacyPackages.x86-64.alejandra;
+    };
+}
